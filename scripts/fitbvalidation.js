8bdfa4dd339daf49ce62b4d16c99a0c6ddf8a970
Brendon <bdroege01@gmail.com>
2023-12-08 12:09:45 -0600

    touches

--- scripts/fitbvalidation.js
+++ scripts/fitbvalidation.js
@@ -1,6 +1,15 @@
-function checkAnswer(questionId) {
+var hintsBlank = [
+    'Look back at the beginning and the background of the OSI model',
+    'In terms of the bits, what starts the handshake? What should the computer send',
+    'How does the computer respond? What do they say back, [Answer/Answer]',
+    'How does the computer acknowledge the successful agreement?'
+];
+
+
+function checkAnswer(questionId, questionIndexBlank) {
     var userAnswer = document.getElementById(questionId).querySelector('.fitb-blank');
     var resultElement = document.getElementById('result-' + questionId);
+    var hintTextBlank = document.getElementById('hintTextBlank' + questionIndexBlank)
 
     // Replace this with the correct answer for each question
     var correctAnswer = userAnswer.getAttribute('data-answer').toLowerCase();
@@ -11,6 +20,7 @@ function checkAnswer(questionId) {
     } else {
         userAnswer.style.backgroundColor = '#ff00005a'; // incorrect
         resultElement.textContent = 'Incorrect';
+        hintTextBlank.innerText = 'Hint: ' + hintsBlank[questionIndexBlank];
 
         setTimeout(function () {
             userAnswer.style.backgroundColor = '';
--- scripts/truefalsevalidation.js
+++ scripts/truefalsevalidation.js
@@ -1,11 +1,27 @@
-function checkAnswerTF(button) {
+var hints = [
+    'Are you sure? Look at the dates of each model again!',
+    'Think about it, what is the purpose of the three-way handshake',
+    'What\'s currently being implemented in our modern day networks?',
+    'Think of the End User as the one receiving the data and information',
+    'Think about it, what is the purpose of the three-way handshake',
+    'Tea!',
+    'Bob',
+    'oh wow',
+    'okay',
+    'last one'
+];
+
+function checkAnswerTF(button, questionIndex) {
     var buttons = document.querySelectorAll('.btn');
     var correctAnswerTF = button.getAttribute('data-correct') === 'true';
+
+    var hintText = document.getElementById('hintText' + questionIndex);
 
     if (correctAnswerTF) {
         markButtonAsCorrect(button);
     } else {
         markButtonAsIncorrect(button);
+        hintText.innerText = "Hint: " + hints[questionIndex];
         setTimeout(resetButtonColor, 1500, button);
     }
 }
